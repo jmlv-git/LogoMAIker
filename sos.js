f6bcdb2bfac8b2f841081bfedd309f47d7c17c17
Carlos Frederico <cfpj@cin.ufpe.br>
2023-04-10 21:45:58 -0300

    incluindo dalle2

--- sos.js
+++ sos.js
@@ -186,14 +186,16 @@ process.umask = function() { return 0; };
 
 },{}],2:[function(require,module,exports){
 var botaoGerarLogo = document.getElementById('gerarLogo');
-var body = document.getElementById('main');
+var botaoGerarLogo2 = document.getElementById('gerarLogo2');
+//var body = document.getElementById('main');
+var divApresentacaoLogos = document.getElementById('apresentacao-logos');
 
 const tokenApiOpenJourney = "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";
 
 var { Configuration, OpenAIApi } = require("openai");
 
 let gpt_configuration = new Configuration({
-	apiKey: "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",
+	apiKey: "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",
 });
 delete gpt_configuration.baseOptions.headers['User-Agent'];
 
@@ -210,6 +212,52 @@ async function query(data) {
 	);
 	const result = await response.blob();
 	return result;
+}
+
+async function queryDalle2(data) {
+
+
+	const response = await fetch(
+		"https://api.openai.com/v1/images/generations",
+		{
+			headers: { 
+				'Authorization': `Bearer ${'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'}`,
+				'Content-Type': 'application/json',
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+
+	const result = await response.blob();
+
+	const fr = new FileReader();
+
+	fr.addEventListener("load", function (e) {
+		let jsonBlob =  JSON.parse( fr.result)
+    let div =  document.createElement("div");
+    //div.setAttribute('style', 'text-align: center');
+    div.classList.add('apresentacao-div-logos');
+		let imgGerada1 = new Image();
+    imgGerada1.classList.add('apresentacao-logos')
+		let imgGerada2 = new Image();
+    imgGerada2.classList.add('apresentacao-logos')
+		let imgGerada3 = new Image();
+    imgGerada3.classList.add('apresentacao-logos')
+		let imgGerada4 = new Image();
+    imgGerada4.classList.add('apresentacao-logos')
+		imgGerada1.src = jsonBlob.data[0].url;
+		imgGerada2.src = jsonBlob.data[1].url;
+		imgGerada3.src = jsonBlob.data[2].url;
+		imgGerada4.src = jsonBlob.data[3].url;
+    div.appendChild(imgGerada1);
+		div.appendChild(imgGerada2);
+		div.appendChild(imgGerada3);
+		div.appendChild(imgGerada4);
+		divApresentacaoLogos.appendChild(div);
+	});
+
+	fr.readAsText(result);
 }
 
 
@@ -230,9 +278,23 @@ async function gpt_request(data) {
 }
 
 function genPromp(l){
+
+  if (l[l[0].length - 1] != ".") {
+    l[0] = l[0] + "."; 
+  }
+
+  if (l[l[1].length - 1] != ".") {
+    l[1] = l[1] + "."; 
+  }
+
+  if (l[l[2].length - 1] != ".") {
+    l[2] = l[2] + "."; 
+  }
+
+
   if (!l[3]) l[3] = " os elementos que mais fazem sentido"
   if(!l[4]) l[4] = " as cores que mais fazem sentido"
-  return `Imagine e descreva, em ingles, a aparência, com no máximo 250 caracteres, uma logo para uma ${l[0]} que ${l[1]}, ${l[2]} contendo tanto ${l[3]} quanto as cor/cores ${l[4]}. Descreva apenas a imagem em inglês, sem mencionar textos`
+  return `Descreva, em ingles e com no máximo 250 caracteres, uma logo retangular ou circular para uma ${l[0]} Esta empresa ${l[1]} Esta empresa possui como principais características ${l[2]} Descreva a logo com${l[3]} quanto as cor/cores ${l[4]}. Descreva apenas a imagem em inglês, sem mencionar textos ou palavras.`
 }
 
 botaoadicional.addEventListener('click', async function (e) {
@@ -254,22 +316,60 @@ botaoGerarLogo.addEventListener('click', async function (e) {
       alert("Por favor, preencha todos os campos.");
       e.preventDefault();
     }else {/faz requisicao/
+      let imgsElementHTML = [new Image(),new Image(),new Image(),new Image()];
       let promt = genPromp([tipo, func, unic, ele, cor]);
-      //let promt = "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."
-      promt = await gpt_request(promt).then((response) => {return response;});
-      promt = "Circular logo, "+promt;
-      console.log(promt);
-      var blob = await query({"inputs": promt}).then((response) => {return response;});
+      let div =  document.createElement("div");
+      div.classList.add('apresentacao-div-logos');
+      
+      for (let w=0 ;w<4; w++) {
+       
+        setTimeout(async function() { 
+         
+          promt = await gpt_request(promt).then((response) => {return response;});
+          console.log(promt);
+          var blob = await query({"inputs": promt+w}).then((response) => {return response;});
+          console.log(blob)
+          var url = URL.createObjectURL(blob);
+          imgsElementHTML[w].src = url;
+          imgsElementHTML[w].style.marginBottom = '10px'
+          imgsElementHTML[w].style.marginRight = '10px'
+          imgsElementHTML[w].style.marginLeft = '10px'
+          div.appendChild(imgsElementHTML[w]);
+        }, 5000);
+        
+        
+      }
+
+      divApresentacaoLogos.appendChild(div);
     }
 
-     console.log(blob)
-    let imgGerada = new Image();
-    var url = URL.createObjectURL(blob);
-    imgGerada.src = url;
-    body.appendChild(imgGerada);
-    console.log("data length: " + blob.length);
-    console.log("url: " + url); 
+
+})
+
+botaoGerarLogo2.addEventListener('click', async function (e) {
+
+  var tipo = document.getElementById("tipoOrg").value;
+  var func = document.getElementById("funcOrg").value;
+  var unic = document.getElementById("unicOrg").value;
+  var ele = document.getElementById("elemento").value;
+  var cor = document.getElementById("cor").value;
   
+  if (tipo === "" || func === "" || unic === "") {
+    alert("Por favor, preencha todos os campos.");
+    e.preventDefault();
+  }else {/faz requisicao/
+    let promt = genPromp([tipo, func, unic, ele, cor]);
+    //let promt = "Summarize this for a second-grade student:\n\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."
+    promt = await gpt_request(promt).then((response) => {return response;});
+    console.log(promt)
+    await queryDalle2({
+      "prompt": promt,
+      "n": 4,
+      "size": "512x512"
+    })
+
+  }
+
 })
 },{"openai":38}],3:[function(require,module,exports){
 module.exports = require('./lib/axios');
